9c74a90e0c3cb3acb48f07b177d3d28fefbdfc32
Diego Gaspar <diegogaspar.dev.br>
2023-09-03 18:01:52 -0300

    fix: cors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 import { io } from 'socket.io-client'
 
-const socket = io(`http://localhost:8080`)
+const socket = io(`http://localhost:8081`)
 
 interface ITreatment {
   id: string
